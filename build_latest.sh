e4012349cfb3af15d1ba1d08d437b2f8911a2188
asmorodskyi <asmorodskyi@suse.com>
2022-07-04 20:51:47 +0200

    remove full clean

--- build_latest.sh
+++ build_latest.sh
@@ -2,6 +2,5 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ./.env
 pushd ${KERNEL_SOURCES}
-make mrproper
 cp $SCRIPT_DIR/.config $KERNEL_SOURCES
 make -j8
